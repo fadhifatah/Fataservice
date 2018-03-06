3a316f8d7c02728832f1b78cc3f94b9595fa9f33
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-03-07 03:16:29 +0700

    Add allowed hosts

--- Fataservice/settings.py
+++ Fataservice/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '#kpbqhy57-47$_6k47omb0)glwgen7l0_qc21s=agk0*ji7366'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://host21018.proxy.infralabs.cs.ui.ac.id',
+    '172.22.0.98:80',
+    '127.0.0.1:8000',
+]
 
 
 # Application definition
@@ -76,27 +80,27 @@ WSGI_APPLICATION = 'Fataservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd9ef928o48bsjr',
-#         'USER': 'kpbnkfjmkeplil',
-#         'PASSWORD': '171ff01a6e8de4e3f09bafde145cc268b4bc81295d448d37a262342c9a2d52bc',
-#         'HOST': 'ec2-54-221-220-59.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fataservice',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '172.22.0.67',
+        'NAME': 'd9ef928o48bsjr',
+        'USER': 'kpbnkfjmkeplil',
+        'PASSWORD': '171ff01a6e8de4e3f09bafde145cc268b4bc81295d448d37a262342c9a2d52bc',
+        'HOST': 'ec2-54-221-220-59.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fataservice',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '172.22.0.67',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
