ca43f7ae837d6909bf8010709c6ce48e23a747b3
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-03-07 02:53:40 +0700

    Add DB from LB, Fix CSRF problem

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,30 +1,38 @@
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import User
 
 
 # Create your views here.
+@csrf_exempt
 def login(request):
-    users_dummy = User.objects.all()
-    for dummy in users_dummy:
-        username = dummy.username
-        display_name = dummy.display_name
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-    return JsonResponse({'username': username, 'display_name': display_name})
+        User.objects.create(username='1406571842', display_name='Fatah Fadhlurrohman')
+        return JsonResponse({'username': username, 'password': password})
+    return JsonResponse({'status': 'Gagal'})
 
 
+@csrf_exempt
 def register(request):
     return JsonResponse({})
 
 
 def get_users(request):
     if request.method == 'GET':
-        page = request.GET['page']
-        limit = request.GET['limit']
-        return JsonResponse({'page': page, 'limit': limit})
+        users_dummy = User.objects.all()
+        for dummy in users_dummy:
+            username = dummy.username
+            display_name = dummy.display_name
+
+        return JsonResponse({'username': username, 'display_name': display_name})
     else:
         return JsonResponse()
 
 
+@csrf_exempt
 def create_comment(request):
     return JsonResponse({})
 
@@ -37,9 +45,11 @@ def get_comments(request):
     return JsonResponse({})
 
 
+@csrf_exempt
 def delete_comment(request):
     return JsonResponse({})
 
 
+@csrf_exempt
 def update_comment(request):
     return JsonResponse({})
