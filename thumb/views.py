4a828a56fe406d0f711e9854f2458e59bcb960e9
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-03-14 13:18:49 +0700

    Update Lab: thumbnail fix stupid typo

--- thumb/views.py
+++ thumb/views.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # Create your views here.
 def image(request):
-    img = open('data/img/image.jpg', 'rb')
+    img = open('data/img/image.jpg', 'rb').read()
     return HttpResponse(img, content_type='image/jpeg')
 
 
