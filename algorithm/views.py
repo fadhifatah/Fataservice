a49a92bcc4c23b0cc1680d32bdae39c9e388c94c
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-03-24 17:59:07 +0700

    Update algorithm App

--- algorithm/views.py
+++ algorithm/views.py
@@ -6,37 +6,41 @@ from collections import OrderedDict
 
 def plus(request):
     if request.method == 'GET':
-        try:
-            number1 = request.GET.get('x1')
-            number2 = request.GET.get('x2')
-        except ValueError:
-            return JsonResponse(OrderedDict(status=400, description='Bad Request, Wrong Parameters'))
+        number1 = request.GET.get('x1')
+        number2 = request.GET.get('x2')
+
+        if number1 is None or number2 is None:
+            return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Wrong Parameters or '
+                                                                              'Missing Parameters')]))
 
         try:
             number1 = int(number1)
             number2 = int(number2)
         except ValueError:
-            return JsonResponse(OrderedDict(status=400, description='Bad Request, Parameters not Integer'))
+            return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Parameters not Integer or '
+                                                                              'can\'t be Empty')]))
 
         result = number1 + number2
-        return JsonResponse(OrderedDict(status=200, description='OK', result=result))
-    return JsonResponse(OrderedDict(status=400, description='Bad Request, Wrong Method'))
+        return JsonResponse(OrderedDict([('status', 200), ('description', 'OK'), ('result', result)]))
+    return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Wrong Method')]))
 
 
 def times(request):
     if request.method == 'GET':
-        try:
-            number1 = request.GET.get('x1')
-            number2 = request.GET.get('x2')
-        except ValueError:
-            return JsonResponse(OrderedDict(status=400, description='Bad Request, Wrong Parameters'))
+        number1 = request.GET.get('x1')
+        number2 = request.GET.get('x2')
+
+        if number1 is None or number2 is None:
+            return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Wrong Parameters or '
+                                                                              'Missing Parameters')]))
 
         try:
             number1 = int(number1)
             number2 = int(number2)
         except ValueError:
-            return JsonResponse(OrderedDict(status=400, description='Bad Request, Parameters not Integer'))
+            return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Parameters not Integer or '
+                                                                              'can\'t be Empty')]))
 
         result = number1 * number2
-        return JsonResponse(OrderedDict(status=200, description='OK', result=result))
-    return JsonResponse(OrderedDict(status=400, description='Bad Request, Wrong Method'))
+        return JsonResponse(OrderedDict([('status', 200), ('description', 'OK'), ('result', result)]))
+    return JsonResponse(OrderedDict([('status', 400), ('description', 'Bad Request, Wrong Method')]))
